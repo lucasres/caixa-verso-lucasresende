e3b4deb51125e893487d09196470e4d4e3496116
lucasres <lucasresone@gmail.com>
2025-11-14 19:20:11 -0300

    add endpoint de consulta de produtos por perfil

--- src/main/java/br/gov/caixa/caixaverso/repository/ProdutoRepository.java
+++ src/main/java/br/gov/caixa/caixaverso/repository/ProdutoRepository.java
@@ -1,15 +1,22 @@
 package br.gov.caixa.caixaverso.repository;
 
+import java.util.List;
+
 import br.gov.caixa.caixaverso.repository.model.ProdutoModel;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped 
 public class ProdutoRepository implements PanacheRepository<ProdutoModel> {
-    
     public ProdutoModel findByTipo(String tipo) {
         var query = getEntityManager().createNamedQuery("findByTipo", ProdutoModel.class);
         query.setParameter("tipo", tipo);
         return query.getSingleResultOrNull();
+    }
+
+    public List<ProdutoModel> findAllByTipo(String tipo) {
+        var query = getEntityManager().createNamedQuery("findByTipo", ProdutoModel.class);
+        query.setParameter("tipo", tipo);
+        return query.getResultList();
     }
 }
---
+++ src/main/java/br/gov/caixa/caixaverso/rest/ProdutoRest.java
@@ -0,0 +1,44 @@
+package br.gov.caixa.caixaverso.rest;
+
+import java.util.List;
+
+import org.jboss.logging.Logger;
+
+import br.gov.caixa.caixaverso.repository.ProdutoRepository;
+import br.gov.caixa.caixaverso.repository.model.ProdutoModel;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("")
+public class ProdutoRest {
+    @Inject
+    Logger logger = Logger.getLogger(ProdutoRest.class);
+
+    @Inject
+    ProdutoRepository produtoRepository;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/produtos-recomendados/{perfil}")
+    public Response getDeProduto(
+        @PathParam("perfil") String perfil
+    ) {
+        Response.Status status = Response.Status.OK;
+        List<ProdutoModel> produtos = produtoRepository.findAllByTipo(perfil);
+        logger.info("perfil " + perfil);
+
+        if (produtos.isEmpty()) {
+            status = Response.Status.NOT_FOUND;
+        }
+
+        return Response.status(status)
+            .entity(produtos)
+            .build();
+
+    }
+}
